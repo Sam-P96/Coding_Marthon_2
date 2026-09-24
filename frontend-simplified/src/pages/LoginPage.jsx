9f1e2d029483b99229082f651840f0de0ab834a5
Sam-P96 <Samnap.Peo@gmail.com>
2026-09-24 14:29:27 +0300

    chore: Fixes bugs from previous iterations

--- frontend-simplified/src/pages/LoginPage.jsx
+++ frontend-simplified/src/pages/LoginPage.jsx
@@ -1,48 +1,56 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = ({ isAuthenticated, setIsAuthenticated }) => {
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   const navigate = useNavigate();
 
   // THIS IS FOR SIGN UP NAV - IGNORE -
   const signupHandler = async () => {
-    navigate('/signup');
+    navigate("/signup");
   };
 
   const submitHandler = async (e) => {
     e.preventDefault();
     setError(null);
 
-    const respones = await fetch('/api/user/login', {
-      method: 'POST',
+    const response = await fetch("/api/user/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ email, password }),
     });
-  };
+  
 
   if (!response.ok) {
     setError(user.error);
     return;
   }
 
-  localStorage.setItem('user', JSON.stringify(user));
-  console.log('login success, saved in local storage');
+  localStorage.setItem("user", JSON.stringify(user));
+  console.log("login success, saved in local storage");
   setIsAuthenticated(true);
-  navigate('/');
-
+  navigate("/");
+  };
   return (
     <div className="create">
       <h2>Login</h2>
       <form onSubmit={submitHandler}>
         <label>Email Address</label>
-        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label>Password</label>
-        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button>Login</button>
       </form>
       <button onClick={signupHandler}>No Account? Sign Up Here</button>
